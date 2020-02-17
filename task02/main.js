9a7a85ed2f5fc37e4f58ed3addf323d5f33ea1b1
Maria Kalantay <masha.kalantay@gmail.com>
2020-02-17 20:09:27 +0200

    Formation

--- task02/main.js
+++ task02/main.js
@@ -5,7 +5,8 @@ function init() {
 	items.forEach((elem, i, items) => {
 		const imgs = elem.querySelector('img');
 		const currWidth = imgs.clientWidth;
-        const currHeight = imgs.clientHeight;
+		const currHeight = imgs.clientHeight;
+		
 		imgs.style.marginLeft = "-" +  currWidth/2 + "px";
 		imgs.style.marginTop = "-" + currHeight/2 + "px";
 	});
