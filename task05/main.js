6fc40e5ec284169e7ee39a7a8c1e2109ccc92f87
Maria Kalantay <masha.kalantay@gmail.com>
2020-02-20 08:31:24 +0200

    Task05

--- task05/main.js
+++ task05/main.js
@@ -1,5 +1,18 @@
 function init() {
+  let blocks = document.querySelectorAll('.list > li:nth-child(even)');
+  let blocksAll = document.querySelectorAll('.list > li');
+  
+  console.log(blocks);
+  console.log(blocksAll);
+  blocks.forEach((elem) => {
+    elem.classList.add("alt");
+  });
 
+  let span = "<span>" + 1  +  "</span>"
+
+  blocksAll.forEach((elem) => {
+    elem.insertAdjacentHTML("afterBegin", span);
+  });  
 }
 
 init();
