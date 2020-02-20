a4cb746560a39f5c1cee0a69c870f83893b16457
Maria Kalantay <masha.kalantay@gmail.com>
2020-02-20 21:33:45 +0200

    Task05

--- task05/main.js
+++ task05/main.js
@@ -2,8 +2,6 @@ function init() {
   let blocks = document.querySelectorAll('.list > li:nth-child(even)');
   let blocksAll = document.querySelectorAll('.list > li');
   
-  console.log(blocks);
-  console.log(blocksAll);
   blocks.forEach((elem) => {
     elem.classList.add("alt");
   });
