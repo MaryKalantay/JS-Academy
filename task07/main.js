e7ed38cc9d66b01afddf32929f3d86c4083ed9b6
Maria Kalantay <masha.kalantay@gmail.com>
2020-02-26 01:50:12 +0200

    Satrt Task07

--- task07/main.js
+++ task07/main.js
@@ -2,4 +2,42 @@ function init() {
 
 }
 
-init();
+// function init(btn, className) {
+//   let holder = document.querySelector(".holder");
+//   let btn = document.querySelector(btn);
+//   let box = document.querySelector(".holder > .box");
+  
+//   btn.addEventListener("click", function(){
+//     boxHide();
+//     this.addEventListener("click", function(){
+//             boxShow();
+//           })
+//   })
+
+
+//   // btns.forEach((elem, index, array) => {
+//   //   elem.addEventListener("click", function(){
+//   //     boxHide();
+  
+//   //     this.addEventListener("click", function(){
+//   //       boxShow();
+//   //     })
+//   //   });
+//   // })
+
+//   function boxHide() {
+//       box.style.display = "none";
+//       holder.classList.add("active")
+//   }
+  
+//   function boxShow() {
+//     box.style.display = "block";
+//     holder.classList.remove("active")
+//   }
+// }
+
+// // init(); 
+// let btn = document.querySelector(".holder > .btn");
+// btn.addEventListener("click", function(){
+//   init();
+// }
