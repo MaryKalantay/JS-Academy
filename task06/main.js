a8aee69e14a25a75c9d722a96ff1ca39252ef9f1
Maria Kalantay <masha.kalantay@gmail.com>
2020-02-26 01:15:37 +0200

    Task06 Alert

--- task06/main.js
+++ task06/main.js
@@ -1,5 +1,14 @@
 function init() {
-
+  let list = document.querySelectorAll('.list > li');
+  
+  console.log(list);
+  list.forEach((elem, index, array) => {
+    elem.addEventListener("click", function(){
+      let list2 = elem.querySelectorAll(' ul > li');
+      let list2Len = list2.length;
+      alert( index, list2Len);
+    });
+  });
 }
 
 init();
