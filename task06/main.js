b2c738ce1cd3e9fc4828adc77ce94f02e9be75cc
Maria Kalantay <masha.kalantay@gmail.com>
2020-02-26 03:07:22 +0200

    Task07 resolve

--- task06/main.js
+++ task06/main.js
@@ -1,14 +1,5 @@
 function init() {
-  let list = document.querySelectorAll('.list > li');
-  
-  console.log(list);
-  list.forEach((elem, index, array) => {
-    elem.addEventListener("click", function(){
-      let list2 = elem.querySelectorAll(' ul > li');
-      let list2Len = list2.length;
-      alert( index, list2Len);
-    });
-  });
+
 }
 
 init();
