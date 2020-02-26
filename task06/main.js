fb0c2bf1f744be9d80aa1991977bf48b877412cd
Maria Kalantay <masha.kalantay@gmail.com>
2020-02-26 23:01:38 +0200

    Task06 nihera nepoymu

--- task06/main.js
+++ task06/main.js
@@ -1,5 +1,34 @@
 function init() {
+	let list = document.querySelectorAll('.list > li');
+	console.log(list);
 
+	list.forEach((elem, index, array) => {
+		elem.addEventListener("click", function(){
+			let parent = elem.parentNode;
+			let childrens = parent.children;
+			console.log("первый список " + parent);
+			console.log("номер ли " + index);
+			console.log("дети первого списка " + childrens);
+			console.log("длинна первого списка " + childrens.length);
+
+			let innerList = elem.querySelectorAll("ul > li");
+			console.log("внутренний список " + innerList);
+
+			
+		});
+	});
 }
 
 init();
+
+
+// let list = document.querySelectorAll('.list > li');
+	
+//   console.log(list);
+//   list.forEach((elem, index, array) => {
+//     elem.addEventListener("click", function(){
+//       let list2 = elem.querySelectorAll(' ul > li');
+//       let list2Len = list2.length;
+//       alert( index, list2Len);
+//     });
+//   });
