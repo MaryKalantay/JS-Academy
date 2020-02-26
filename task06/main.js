63bcc4fa7b1c6709a3b5dace8a3d961206b5c9a7
Maria Kalantay <masha.kalantay@gmail.com>
2020-02-26 03:28:11 +0200

    Task06 Alert

--- task06/main.js
+++ task06/main.js
@@ -1,14 +1,30 @@
 function init() {
-  let list = document.querySelectorAll('.list > li');
-  
-  console.log(list);
-  list.forEach((elem, index, array) => {
-    elem.addEventListener("click", function(){
-      let list2 = elem.querySelectorAll(' ul > li');
-      let list2Len = list2.length;
-      alert( index, list2Len);
-    });
-  });
+	let list = document.querySelectorAll('.list > li');
+	console.log(list);
+
+	list.forEach((elem, index, array) => {
+		elem.addEventListener("click", function(){
+			alert(index,  liLen(elem));
+		});
+	});
+
+	function liLen(list) {
+		let list2 = list.querySelectorAll('li');
+		let listLen = list2.length;
+		return listLen;
+	};
 }
 
 init();
+
+
+// let list = document.querySelectorAll('.list > li');
+	
+//   console.log(list);
+//   list.forEach((elem, index, array) => {
+//     elem.addEventListener("click", function(){
+//       let list2 = elem.querySelectorAll(' ul > li');
+//       let list2Len = list2.length;
+//       alert( index, list2Len);
+//     });
+//   });
