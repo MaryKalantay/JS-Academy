5083b9b90591146953eaa33d357c0b00588f260c
Maria Kalantay <masha.kalantay@gmail.com>
2020-02-27 07:21:24 +0200

    Task06 nihera nepoymu

--- task06/main.js
+++ task06/main.js
@@ -6,10 +6,11 @@ function init() {
 		elem.addEventListener("click", function(){
 			let parent = elem.parentNode;
 			let childrens = parent.children;
-			console.log("первый список " + parent);
-			console.log("номер ли " + index);
-			console.log("дети первого списка " + childrens);
-			console.log("длинна первого списка " + childrens.length);
+			let childrenLen = childrens.length;
+			console.log(" list " + parent);
+			console.log("list ли " + index);
+			console.log("all  li " + childrens);
+			console.log("длинна list " + childrens.length);
 
 			let innerList = elem.querySelectorAll("ul > li");
 			console.log("внутренний список " + innerList);
