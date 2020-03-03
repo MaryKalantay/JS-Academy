9f9263d6fded29fc36e7d8e037a1dcb660b44217
Maria Kalantay <masha.kalantay@gmail.com>
2020-03-03 20:23:41 +0200

    Task08

---
+++ task08/main.js
@@ -0,0 +1,36 @@
+function init() {
+
+	let visual = document.getElementById('visual');
+	let visualString = visual.innerHTML.toString();
+
+	let vStr = visualString.match(/\d/g);
+	let maxNumber = Math.max(...vStr);
+	console.log(maxNumber);
+	let str = visualString.split(";").pop();
+	let str2 = str.split(/(\d+)/);
+	console.log(str);
+	console.log(str2);
+	console.log(str2[1]);
+	let numberRandom = Math.floor(Math.random() * maxNumber) + 1;
+
+	str2[1] =  numberRandom;
+
+	if (numberRandom < 10) {
+		str2[1] = "0" + numberRandom;
+	} else {
+		str2[1] = numberRandom;
+	}
+
+	let imgPath = str2[0] + str2[1] + str2[2];
+	console.log(imgPath);
+	let img =  document.createElement('img');
+	let imgAtrr =  img.setAttribute("src", imgPath);
+	console.log(visual);
+
+	visual.innerHTML = img;
+	document.body.appendChild(img);
+
+	return visual;
+}
+
+init();
