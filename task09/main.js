fdd155cd5f23d337058e712c0664de6798b9b7be
Maria Kalantay <masha.kalantay@gmail.com>
2020-03-04 03:18:56 +0200

    Task09

--- task09/main.js
+++ task09/main.js
@@ -1,5 +1,19 @@
 function init() {
 
+	let btn = document.getElementById('btn');
+	let item = document.querySelectorAll('ul > li');
+
+
+
+  item.forEach((li, index, array) => {
+    if(li.className == "active") {
+      btn.onclick = () => {
+        btn.innerHTML = "Stop";
+        li.classList = "";
+        li.nextElementSibling.classList = "active";
+      }
+    }
+  });
 }
 
 init();
