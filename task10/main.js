f5b2a2ad877b448f14b3ef93f6a473f6524f871b
Maria Kalantay <masha.kalantay@gmail.com>
2020-03-14 01:25:57 +0200

    Task10 resolve

--- task10/main.js
+++ task10/main.js
@@ -1,5 +1,20 @@
+let contentPosition = document.getElementById("content").offsetTop;
+let sidebar = document.getElementById("sidebar");
+let sidebarHeight = sidebar.offsetHeight;
+let footerPosition = document.getElementById("footer").offsetTop;
+
+let maxPosition = footerPosition - sidebarHeight;
+
 function init() {
-
+  if(window.scrollY >= contentPosition ) {
+    sidebar.className = "active";
+    if (window.scrollY > maxPosition) {
+      sidebar.className = "";
+    }
+  }
+  else {
+    sidebar.className = "";
+  }
 }
-
 init();
+window.onscroll = function() {init()};
