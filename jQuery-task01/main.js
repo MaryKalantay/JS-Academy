ecca0c1df36f48394f9d77245471aff08e9b7639
Maria Kalantay <masha.kalantay@gmail.com>
2020-03-15 01:16:41 +0200

    jQ-t01 resolve

--- jQuery-task01/main.js
+++ jQuery-task01/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   $(".button").click(function(){
-    $(this).parent().toggleClass("opened").find(".element").css("background-color", "red");
+    let parentBtn = $(this).parent();
+    let element = $(this).parent().toggleClass("opened").find(".element");
+
+    if(parentBtn.hasClass( "opened" )) {
+      element.css("background-color", "red");
+    }
+    else {
+      element.css("background-color", "transparent");  
+    }
   });
 });
+
