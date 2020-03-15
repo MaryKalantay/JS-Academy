3a406f6ddb75149f45bb7ccd0e84e24adfc75310
Maria Kalantay <masha.kalantay@gmail.com>
2020-03-15 03:03:48 +0200

    jQ-t02 resolve but has question

--- jQuery-task02/main.js
+++ jQuery-task02/main.js
@@ -1,5 +1,13 @@
-function init() {
+let allInputs = $(":input");
 
-}
-
-init();
+allInputs.each(function(  index ) {
+	let defaultValue = $( this ).val();
+	$( this ).focus(function() { 
+		$( this ).val("");
+	});
+	$( this ).blur(function(){
+		if(!$(this).val()){
+			$( this ).val(defaultValue); 
+		}
+	});
+});
