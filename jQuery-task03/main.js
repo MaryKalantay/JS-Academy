a9e07f2ef7fbc3c627ff5c7fadc60b4f3023a78d
Maria Kalantay <masha.kalantay@gmail.com>
2020-03-24 22:48:04 +0200

    jQ-t03 resolve

--- jQuery-task03/main.js
+++ jQuery-task03/main.js
@@ -1,5 +1,4 @@
-function init() {
-
-}
-
-init();
+$(".list > li").click(function() {
+  let rholderThis = $(this).find('.rholder').css("display", "block");
+  let rholder = $(this).siblings().find('.rholder').css("display", "none");
+});
