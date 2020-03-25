4b096ee82a68c95f2b51f02e4be67d5f0757de30
Maria Kalantay <masha.kalantay@gmail.com>
2020-03-26 01:04:46 +0200

    jQ-t04

--- jQuery-task04/main.js
+++ jQuery-task04/main.js
@@ -1,5 +1,36 @@
+jQuery(function() {
+	init();
+});
+
 function init() {
+	let tooltip = $('<div id="tooltip">');
+	tooltip.appendTo('body');
+	let links = $('a');
 
+	jQuery.each( links, function( i, val ) {
+		if( $(this).attr( "title") ) {
+
+			let atribute = $(this).attr( "title");
+			console.log(atribute);
+
+			$(this).removeAttr( "title" );
+			$(this).mouseenter(function() {
+				tooltip.text(atribute).css("display", "inline-block");
+				console.log(tooltip);
+			});
+
+			$("body").mousemove(function( event ) {
+				let pageX = event.pageX.toString() + "px";
+				let pageY = event.pageY.toString() + "px";
+		
+				tooltip.css( "left", pageX);
+				tooltip.css("top", pageY);
+				tooltip.css("margin-left", "10px");
+			});
+			 
+			$("body").mouseleave(function() {
+				tooltip.css("display", "none");	
+			});
+		}
+	});
 }
-
-init();
