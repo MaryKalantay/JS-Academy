137d112697c0919512f4444dd723adffc15b9a9f
Maria Kalantay <masha.kalantay@gmail.com>
2020-04-16 11:38:31 +0300

    move 1px?

--- jQuery-task05/main.js
+++ jQuery-task05/main.js
@@ -3,5 +3,42 @@ jQuery(function() {
 });
 
 function init() {
+	$(document).keydown(function(e){
+		console.log(e);
+
+		let topTank = 0;
+		let  leftTank = 0;
+		let rightTank = 0;
+		let bottomTank = 0;
+		
+		if(e.keyCode == 37) {
+			tank.css('background', 'red');
+			leftTank++;
+			let leftPos = leftTank.toString() + "px";
+			console.log(leftPos);
+			tank.css('left', leftPos);
+		}
+
+		else if (e.keyCode == 39) {
+			tank.css('background', 'blue');
+		}
+
+		else if (e.keyCode == 38) {
+			tank.css('background', 'green');
+		}
+
+		else if (e.keyCode == 40) {
+			tank.css('background', 'brown');
+		}
+	});
+
+	let tank = $(".box");
+
+	$('div.box-holder').mousedown(function(e){
+		tank.css('left', e.pageX);
+		tank.css('top', e.pageY);
+		tank.css('transform', 'translate(-120px, 0%)')
+	});
+
 
 }
