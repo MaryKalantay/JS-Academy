413798ad450452abfb968e3922abe55c9a348c4b
Maria Kalantay <masha.kalantay@gmail.com>
2020-04-18 04:08:43 +0300

    jQ-t05 resolve

--- jQuery-task05/main.js
+++ jQuery-task05/main.js
@@ -9,29 +9,29 @@ function init() {
 		if(e.keyCode == 37) {
 			tank.css('background', 'red');
 			tank.animate({
-        left: "-=10px"
-      }, 'fast');
+		left: "-=10px"
+	  }, 'fast');
 		}
 
 		else if (e.keyCode == 39) {
 			tank.css('background', 'blue');
 			tank.animate({
-        left: "+=10px"
-      }, 'fast');
+		left: "+=10px"
+	  }, 'fast');
 		}
 
 		else if (e.keyCode == 38) {
 			tank.css('background', 'green');
 			tank.animate({
-        top: "-=10px"
-      }, 'fast');
+		top: "-=10px"
+	  }, 'fast');
 		}
 
 		else if (e.keyCode == 40) {
 			tank.css('background', 'brown');
 			tank.animate({
-        top: "+=10px"
-      }, 'fast');
+		top: "+=10px"
+	  }, 'fast');
 		}
 	});
 
